22b8aa4ef37b351b84a6327fa6b1ffc20619a7a4
QuangTV <vanquang6789@gmail.com>
2024-10-01 14:18:19 +0700

    feature: setup mongo

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(port, () => {
-    console.log("Server running on port: " + port);
-});
+mongoose.connect(process.env.MONGO_CONNECTION_STRING || "")
+    .then(() => {
+        console.log("Connected mongo DB");
+        app.listen(port, () => {
+            console.log("Server running on port: " + port);
+        });
+    })
+    .catch(console.error);
