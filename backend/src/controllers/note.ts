336d88586ae4df3099b064e7a19b082c46325d92
QuangTV <vanquang6789@gmail.com>
2024-10-02 12:26:31 +0700

    2.3 get note detail

--- backend/src/controllers/note.ts
+++ backend/src/controllers/note.ts
@@ -1,10 +1,27 @@
 import { RequestHandler } from "express";
 import NoteModel from "../models/note";
+import mongoose from "mongoose";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
     try {
         const notes = await NoteModel.find().exec();
         res.status(200).json(notes);
+    } catch(error) {
+        next(error);
+    }
+}
+
+export const getNote: RequestHandler = async (req, res, next) => {
+    const noteId = req.params.noteId;
+    try {
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw Error("noteId is invalid object id");
+        }
+        const note = await NoteModel.findById(noteId).exec();
+        if (!note) {
+            throw Error("note not found");
+        }
+        res.status(200).json(note);
     } catch(error) {
         next(error);
     }
