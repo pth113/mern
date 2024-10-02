18bea3c940d748862ad0d51f8388a385f7f43e9e
QuangTV <vanquang6789@gmail.com>
2024-10-02 11:31:30 +0700

    2.2 api create new note

--- backend/src/controllers/note.ts
+++ backend/src/controllers/note.ts
@@ -9,3 +9,30 @@ export const getNotes: RequestHandler = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getNote: RequestHandler = async (req, res, next) => {
+    const noteId = req.params.noteId;
+    try {
+        const notes = await NoteModel.findById(noteId).exec();
+        res.status(200).json(notes);
+    } catch(error) {
+        next(error);
+    }
+}
+
+interface NoteRequestBody  {
+    title : string,
+    text? : string,
+}
+
+export const createNote: RequestHandler<unknown, unknown, NoteRequestBody, unknown> = async (req, res, next) => {
+    const {title, text} = req.body;
+    try {
+        const newNote = await NoteModel.create({
+            title, text
+        });
+        res.status(201).json(newNote);
+    } catch(error) {
+        next(error);
+    }
+}
