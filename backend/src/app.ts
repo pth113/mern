1c31a54f2050fa4274eba2e796c4a88e8f29db27
QuangTV <vanquang6789@gmail.com>
2024-10-13 20:58:28 +0700

    using http error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import NoteRoutes from "./routes/notes";
+import createHttpError, {isHttpError} from "http-errors";
 
 const app = express();
 
@@ -7,17 +8,19 @@ app.use(express.json());
 app.use("/api/notes", NoteRoutes);
 
 app.use((req, res, next) => {
-    next(Error("Endpoint not found"));
+    next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req:Request, res: Response, next: NextFunction) => {
     console.error(error);
     let message = "Unknow error";
-    if (error instanceof Error) {
+    let statusCode = 500;
+    if (isHttpError(error)) {
         message = error.message;
+        statusCode = error.status;
     }
-    res.status(500).json({error: message});
+    res.status(statusCode).json({error: message});
 });
 
 export default app;
